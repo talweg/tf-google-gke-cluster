b17af31a544c47a4ffdc446d6f4a65067c207156
Serhii <talweg.ua@gmail.com>
2023-06-08 11:31:56 +0300

    add main.tf

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.52.0"
+    }
+  }
+}
+
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project name"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "europe-central2"
+  description = "GCP region name"
+}
+
+provider "google" {
+  # Configuration options
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "g1-small"
+  description = "machine type"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = 2
+  description = "node pull"
+}
+
+resource "google_container_cluster" "demo" {
+  name     = "demo"
+  location = var.GOOGLE_REGION
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "main" {
+  name       = "main"
+  project    = google_container_cluster.demo.project
+  cluster    = google_container_cluster.demo.name
+  location   = google_container_cluster.demo.location
+  node_count = var.GKE_NUM_NODES
+
+  node_config {
+    machine_type = var.GKE_MACHINE_TYPE
+  }
+}
